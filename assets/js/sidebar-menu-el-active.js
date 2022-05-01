11a13c21f705b689480635b279da8782e848650a
TimofeevAnton1980 <Maestroat@yandex.ru>
2022-05-01 16:59:58 +0300

    FINAL-5

--- assets/js/sidebar-menu-el-active.js
+++ assets/js/sidebar-menu-el-active.js
@@ -5,18 +5,19 @@ let menuBurgerEl = document.querySelectorAll(
   '.burger-menu__list > .burger-menu__list-el',
 );
 const component = document.querySelector('.page__main-component-title');
-menuEl = [...menuEl];
-menuBurgerEl = [...menuBurgerEl];
-menuEl.forEach(item => {if (item.innerText.toLowerCase() === component.innerText.toLowerCase()) {
-	item.childNodes[1].classList.add('sidebar-menu__href_active');
-}});
-menuBurgerEl.forEach(item => {if (item.innerText.toLowerCase() === component.innerText.toLowerCase()) {
-	item.childNodes[1].classList.add('burger-menu__list-el-href_active');
-}})
-// for (let i = 0; i < menuEl.length; i++) {
-//   if (menuEl[i].innerText.toLowerCase() === component.innerText.toLowerCase()) {
-//     console.log(menuBurgerEl[i].childNodes[1]);
-//     menuEl[i].childNodes[1].classList.add('sidebar-menu__href_active');
-//     menuBurgerEl[i].childNodes[1].classList.add('burger-menu__list-el-href_active');
-//   }
-// }
+if (menuEl) {
+  menuEl = [...menuEl];
+  menuEl.forEach((item) => {
+    if (item.innerText.toLowerCase() === component.innerText.toLowerCase()) {
+      item.firstElementChild.classList.add('sidebar-menu__href_active');
+    }
+  });
+}
+if (menuBurgerEl) {
+  menuBurgerEl = [...menuBurgerEl];
+  menuBurgerEl.forEach((item) => {
+    if (item.innerText.toLowerCase() === component.innerText.toLowerCase()) {
+      item.firstElementChild.classList.add('burger-menu__list-el-href_active');
+    }
+  });
+}
